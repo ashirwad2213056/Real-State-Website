79a41d48395736a78ae783d1afc107eb433186aa
ashirwad2213056 <ashirwad999999999@gmial.com>
2025-04-27 20:37:25 +0530

    Completed About Page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='max-w-6xl mx-auto p-4'>
+      <h1 className='text-3xl font-bold mb-4 my-8'>
+        About Us
+      </h1>
+      <p className='mb-4 text-slate-800'>
+        Welcome to our real estate website! We are dedicated to helping you find your dream home or investment property. Our team of experienced professionals is here to guide you through every step of the buying or selling process. Whether you're a first-time buyer or a seasoned investor, we have the resources and expertise to meet your needs. Thank you for choosing us as your trusted real estate partner!
+      </p>
+      <h2 className='text-3xl font-bold mb-4 my-8'>Our Mission</h2>
+      <p className='mb-4 text-slate-800'>
+        Our mission is to provide exceptional service and support to our clients, ensuring a smooth and successful real estate experience. We strive to build lasting relationships based on trust, integrity, and professionalism.</p>
+    </div>
   )
 }
